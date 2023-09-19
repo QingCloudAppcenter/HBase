32b6ca8bc71b9c79618f780d89a724ea0e33bfd4
jackyu <jackyu@yunify.com>
2023-09-19 12:41:46 +0800

    add custom service start-hdfs-balancer

--- src/scripts/make_image.sh
+++ src/scripts/make_image.sh
@@ -55,7 +55,7 @@ wget ${URL}/app-agent-linux-amd64.tar.gz
 tar -xzvf app-agent-linux-amd64.tar.gz; cd app-agent-linux-amd64; sh install.sh; cd ${HOME_DIR}; rm -rf app-agent-linux-amd64*
 
 # setup python with python2
-apt update && apt install -y python2 && ln -s /usr/bin/python2 /usr/bin/python && apt clean
+apt update && apt install -y python2 jq && ln -s /usr/bin/python2 /usr/bin/python && apt clean
 
 echo "* soft nproc 65535" >> /etc/security/limits.conf
 echo "* hard nproc 65535" >> /etc/security/limits.conf
